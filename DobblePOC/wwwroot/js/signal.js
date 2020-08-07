b69d4a1d9a82941448f8eccbec4c4b38d2d0f9d7
newtomsoft <thomas@newtomsoft.fr>
2020-08-07 19:37:34 +0200

    simplification fichiers signal en 1

--- DobblePOC/wwwroot/js/signal.js
+++ DobblePOC/wwwroot/js/signal.js
@@ -1,54 +1,61 @@
-﻿var ConnectionHubGame;
+﻿//Hub Connection
+var ConnectionHubGame;
 function CallSignalR() {
     ConnectionHubGame = new signalR.HubConnectionBuilder().withUrl("/hubGame").withAutomaticReconnect().build();
-    ConnectionHubGame.start()
-        .then(function () { })
-        .catch(function (err) {
-            return console.error(err.toString());
-        });
-
-    ConnectionHubGame.on("ReceivePlayerInGame", function (pseudos) {
-        ReceivePlayerInGame(pseudos);
-    });
-
-    ConnectionHubGame.on("ReceiveStartGame", function (centerCard) {
-        ReceiveStartGame(centerCard);
-    });
-
-    ConnectionHubGame.on("ReceiveChangeCenterCard", function (pseudo, centerCard) {
-        ReceiveChangeCenterCard(pseudo, centerCard);
-    });
-
-    ConnectionHubGame.on("ReceiveGameFinished", function (pseudo) {
-        ReceiveGameFinished(pseudo);
-    });
+    ConnectionHubGame.start().catch(function (err) { return console.error(err.toString()); });
+    ConnectionHubGame.on("ReceivePlayersInGame", function (pseudos) { ReceivePlayersInGame(pseudos); });
+    ConnectionHubGame.on("ReceiveAdditionalDeviceInGame", function (additionalDevices) { ReceiveAdditionalDeviceInGame(additionalDevices); });
+    ConnectionHubGame.on("ReceiveStartGame", function (centerCard) { ReceiveStartGame(centerCard); });
+    ConnectionHubGame.on("ReceiveChangeCenterCard", function (pseudo, centerCard) { ReceiveChangeCenterCard(pseudo, centerCard); });
+    ConnectionHubGame.on("ReceiveGameFinished", function (pseudo) { ReceiveGameFinished(pseudo); });
 }
 
 //Send
 function SendPlayerInGame() {
-    ConnectionHubGame.invoke("HubPlayerInGame", GameId, Pseudo).catch(function (err) { return console.error(err.toString()); });
+    ConnectionHubGame.invoke("HubPlayerInGame", GameId, ThisPseudo).catch(function (err) { return console.error(err.toString()); });
 };
+
+function SendAdditionalDeviceInGame() {
+    ConnectionHubGame.invoke("HubAdditionalDeviceInGame", GameId, ThisAdditionalDevice).catch(function (err) { return console.error(err.toString()); });
+};
+
 
 function SendStartGame(centerCard) {
     ConnectionHubGame.invoke("HubStartGame", centerCard).catch(function (err) { return console.error(err.toString()); });
 }
 
 function SendChangeCenterCard(centerCard) {
-    ConnectionHubGame.invoke("HubChangeCenterCard", Pseudo, centerCard).catch(function (err) { return console.error(err.toString()); });
+    ConnectionHubGame.invoke("HubChangeCenterCard", ThisPseudo, centerCard).catch(function (err) { return console.error(err.toString()); });
 };
 
 function SendGameFinished() {
-    ConnectionHubGame.invoke("HubGameFinished", Pseudo).catch(function (err) { return console.error(err.toString()); });
+    ConnectionHubGame.invoke("HubGameFinished", ThisPseudo).catch(function (err) { return console.error(err.toString()); });
 };
 
 //Receive
-async function ReceivePlayerInGame(pseudos) {
-    ShowNewPlayerInGame(pseudos);
+async function ReceivePlayersInGame(pseudos) {
+    ShowPlayersInGame(pseudos);
+}
+
+async function ReceiveAdditionalDeviceInGame(additionalDevices) {
+    AdditionalDevices = additionalDevices;
+    $('#centerCard').hide();
+    //ShowAdditionalDevicesInGame(additionalDevices);
+
+    //todo autre chose ?
 }
 
 async function ReceiveStartGame(centerCard) {
     GetCenterCard(centerCard);
-    GetCards();
+    if (ThisPlayerGuid !== undefined)
+        GetCards();
+    else {
+        $('#startGame').hide();
+        $('#startGameWait').hide();
+        ShowCenterCard();
+        InitPlayersInfos();
+        ShowPlayersInfos();
+    }
 }
 
 async function ReceiveChangeCenterCard(pseudo, centerCard) {
